ded36214eb2086ebf3b7b3f2a63331128120be86
loic-pottierbarre <loic.POTTIER-BARRE@etu.univ-amu.fr>
2018-01-15 14:05:30 +0100

    Ajout librairies gettext + modif navbar

--- view/frontend/nav.php
+++ view/frontend/nav.php
@@ -1,20 +1,20 @@
 <?php ob_start(); ?>
     <nav class="navbar navbar-expand-lg navbar-toggleable-sm navbar-dark">
 
-	            <a class="navbar-brand" href="#">Navbar</a>
+	            <a class="navbar-brand" href="#">Logo</a>
 	            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 	            <span class="navbar-toggler-icon custom-toggler"></span>
 	            </button>
 	            <div class="collapse navbar-collapse navbar-toggleable-md justify-content" id="navbarSupportedContent">
 	                <ul class="nav nav-pills navbar-nav ml-auto ">
 	                    <li class="nav-item py-1">
-	                        <a class="nav-link px-3" href="#">Sign up</a>
+	                        <a class="nav-link px-3" href="index.php?action=signup"><?= _("Sign up")?></a>
 	                    </li>
 	                    <li class="nav-item py-1">	                        
-	                        <a class="nav-link px-3" href="index.php?action=signin">Sign in</a>
+	                        <a class="nav-link px-3" href="index.php?action=signin"><?= _("Sign in") ?></a>
 	                    </li>
 	                    <li class="nav-item py-1 ">
-	                        <a class="nav-link px-3" href="#">Translate</a>
+	                        <a class="nav-link px-3" href="#"><?= _("Translate") ?></a>
 	                    </li>
 	                    <li class="nav-item dropdown py-1">
 							<a href="#" class="nav-link px-3 dropdown-toggle" data-toggle="dropdown"><i class="fa fa-list"></i>Lang<b class="caret"></b></a>
