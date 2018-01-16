502cf87ee690bf921f70157434e2c793b6d37fc8
William-Barry-Aix <william.barry@etu.univ-amu.fr>
2018-01-16 19:20:10 +0100

    correction du systeme de sessions

--- view/frontend/nav.php
+++ view/frontend/nav.php
@@ -20,36 +20,36 @@
                         <?php if (isset($_SESSION['type'])): ?>
                             <?php if ($_SESSION['type'] == 0): ?>
                                 <li class="nav-item dropdown py-1">
-                                    <a href="#" class="nav-link px-3 dropdown-toggle" data-toggle="dropdown"><i class="fa fa-list"></i>Option<b class="caret"></b></a>
+                                    <a href="#" class="nav-link px-3 dropdown-toggle" data-toggle="dropdown"><i class="fa fa-list"></i><?= _("Options") ?><b class="caret"></b></a>
                                     <ul class="dropdown-menu py-0  my-0 border-0">
-                                        <li class="px-3  nav-item"><a class="nav-link" href="index.php?action=compte">Compte</a></li>
-                                        <li class="px-3  nav-item"><a class="nav-link" href="index.php?action=deconnexion">Deconnexion</a></li>
+                                        <li class="px-3  nav-item"><a class="nav-link" href="index.php?action=compte"><?= _("Account") ?></a></li>
+                                        <li class="px-3  nav-item"><a class="nav-link" href="index.php?action=deconnexion"><?= _("Disconnection") ?></a></li>
                                     </ul>
                                 </li>
                             <?php endif; ?>
                             <?php if ($_SESSION['type'] == 1): ?>
                                 <li class="nav-item dropdown py-1">
-                                    <a href="#" class="nav-link px-3 dropdown-toggle" data-toggle="dropdown"><i class="fa fa-list"></i>Option<b class="caret"></b></a>
+                                    <a href="#" class="nav-link px-3 dropdown-toggle" data-toggle="dropdown"><i class="fa fa-list"></i><?= _("Options") ?><b class="caret"></b></a>
                                     <ul class="dropdown-menu py-0  my-0 border-0">
-                                        <li class="px-3  nav-item"><a class="nav-link" href="index.php?action=compte">Compte</a></li>
-                                        <li class="px-3  nav-item"><a class="nav-link" href="index.php?action=translation">Ajouter Traduction</a></li>
-                                        <li class="px-3  nav-item"><a class="nav-link" href="index.php?action=deconnexion">Deconnexion</a></li>
+                                        <li class="px-3  nav-item"><a class="nav-link" href="index.php?action=compte"><?= _("Account") ?></a></li>
+                                        <li class="px-3  nav-item"><a class="nav-link" href="index.php?action=translation"><?= _("Add translation") ?></a></li>
+                                        <li class="px-3  nav-item"><a class="nav-link" href="index.php?action=deconnexion"><?= _("Disconnection") ?></a></li>
                                     </ul>
                                 </li>
                             <?php endif; ?>
                             <?php if ($_SESSION['type'] == 2): ?>
                                 <li class="nav-item dropdown py-1">
-                                    <a href="#" class="nav-link px-3 dropdown-toggle" data-toggle="dropdown"><i class="fa fa-list"></i>Option<b class="caret"></b></a>
+                                    <a href="#" class="nav-link px-3 dropdown-toggle" data-toggle="dropdown"><i class="fa fa-list"></i><?= _("Options") ?><b class="caret"></b></a>
                                     <ul class="dropdown-menu py-0  my-0 border-0">
-                                        <li class="px-3  nav-item"><a class="nav-link" href="index.php?action=compte">Compte</a></li>
-                                        <li class="px-3  nav-item"><a class="nav-link" href="index.php?action=gererSite">Gestion Site</a></li>
-                                        <li class="px-3  nav-item"><a class="nav-link" href="index.php?action=deconnexion">Deconnexion</a></li>
+                                        <li class="px-3  nav-item"><a class="nav-link" href="index.php?action=compte"><?= _("Account") ?></a></li>
+                                        <li class="px-3  nav-item"><a class="nav-link" href="index.php?action=gererSite"><?= _("Manage website") ?></a></li>
+                                        <li class="px-3  nav-item"><a class="nav-link" href="index.php?action=deconnexion"><?= _("Disconnection") ?></a></li>
                                     </ul>
                                 </li>
                             <?php endif; ?>
                         <?php endif; ?>
 	                    <li class="nav-item dropdown py-1">
-							<a href="#" class="nav-link px-3 dropdown-toggle" data-toggle="dropdown"><i class="fa fa-list"></i>Lang<b class="caret"></b></a>
+							<a href="#" class="nav-link px-3 dropdown-toggle" data-toggle="dropdown"><i class="fa fa-list"></i><?= _("Lang") ?><b class="caret"></b></a>
 							<ul class="dropdown-menu py-0  my-0 border-0">
 								<li class="px-3  nav-item"><a class="nav-link" href="#">Fra</a></li>
 								<li class="px-3  nav-item"><a class="nav-link" href="#">Ang</a></li>
