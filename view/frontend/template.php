b9d4839a36c7107a6270469b7a8571105f591618
jb <jean-baptiste.pezon-chanut@etu.univ-amu.fr>
2018-01-28 23:32:47 +0100

    Ajout des mentions legal

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -39,6 +39,9 @@ $meta
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script> 
     </div>
 </body>
+<footer>
+    <p><?= $tradList["Legal Notice: Using bootstrap and Php Manual"] ?></p>
+</footer>
 <meta name="description" content="<?= $meta ?>" />
 <meta name="keywords" content="HTML,CSS,TRADUCTION,PHP,SQL" />
 </html>
